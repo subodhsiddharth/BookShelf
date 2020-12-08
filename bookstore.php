012f3106df6cda7e13d0afd45506a12972ba9af1
subodhsiddharth <2019155@iiitdmj.ac.in>
2020-12-09 00:31:11 +0530

    Create bookstore.php

---
+++ bookstore.php
@@ -0,0 +1,214 @@
+
+<!DOCTYPE >
+<html>
+<head>
+<i src="images/logo.jpg"></i>
+<title>
+    BookShelf : new generation library
+</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel = "icon" href ="images/logo.jpg"  type = "image/x-icon"> 
+<link rel="stylesheet" type="text/css" href="style/style_4.css">
+</head>
+<body>
+<div id="header" >                                           <!--do not touch-->
+    <table border=0px height="8%" align="Center" width="100%">
+        <TR>
+        <TD width="2%">
+        </TD>
+        <TD width="3%">
+            <img width="100%" src="images/logo.png" align="Left" id="logo">
+        </TD>
+        <TD width="2%">
+            
+        </TD>
+        <TD width="5%">
+            BookShelf 
+        </TD>
+        <TD width="48%">    
+        </TD>
+        <TD width="5%">
+            <a href="signup.html" class="Join"><button >Join</button></a>
+        </TD>
+        <TD width="5%">
+            <a href="signin.html" class="logIn"><button >Log In</button></a>
+        </TD>
+        <TD width="2%">
+            <div id="TopOption">
+            <button><img src="images/option.jpg"></button>
+            </div>
+        </TD>
+    </TR>
+    </table>  
+</div>
+<div id="TempHead">
+    <table border=0px height="10%" align="Center">
+    <TR>
+        <TD width="2%">
+        </TD>
+        <TD width="5%" id="flexopen">
+            <button><img src="images/option.jpg"></button>
+        </TD>
+        <TD width="6%">
+            <a href="aboutus.html"> About Us</a>
+        </TD>
+        <TD width="7%">
+           <a href="aboutus.html"> Contact Us</a>
+        </TD>
+        <TD width="3%">
+            
+        </TD>
+        <TD width="66%"> 
+        </TD>
+        <TD width="5%">
+        </TD>
+        
+    </TR>
+    </table>    
+</div>
+<div class="headspace">
+                                                                <!--space below fixed header of page do not touch-->
+</div>
+
+<div id="flex-box">
+    <button id="cross"><img src="images/cross.jpg"></button>
+            INDEX
+    <div>Most Rated</div>
+    <div>New Writers</div>
+    <div>Novels</div>
+    <div>Language</div>
+    <div>Education</div>
+    <div>Fiction</div>
+    
+</div>
+
+<div id="profile">
+            Profile
+    <div>Most Rated</div>
+    <div>New Writers</div>
+    <div>Novels</div>
+    <div>Language</div>
+    <div>Education</div>
+    <div>Fiction</div>
+    
+</div>
+
+<div id="main">               <!--this box contains running booklist-->
+
+<div id="BookOfMonth">
+    <h1>All Books </h1>
+    <div id="books">
+        <div>Most Rated</div>
+        <div>New Writers</div>
+        <div>Novels</div>
+        <div>Language</div>
+        <div>Education</div>
+        <div>Fiction</div>
+        <div>Most Rated</div>
+        <div>New Writers</div>
+        <div>Novels</div>
+        <div>Language</div>
+        <div>Education</div>
+        <div>Fiction</div>
+        <div>Most Rated</div>
+        <div>New Writers</div>
+        <div>Novels</div>
+        <div>Language</div>
+        <div>Education</div>
+        <div>Fiction</div>
+        <div>Most Rated</div>
+        <div>New Writers</div>
+        <div>Novels</div>
+        <div>Language</div>
+        <div>Education</div>
+        <div>Fiction</div>
+        <div>Fiction</div>
+    </div>
+</div>
+
+    <div id="BookOfMonth">
+        <h1>Most Rated Books</h1>
+        <div id="books">
+            <div>Most Rated</div>
+            <div>New Writers</div>
+            <div>Novels</div>
+            <div>Language</div>
+            <div>Education</div>
+            <div>Fiction</div>
+        </div>
+    </div>
+<div id="BookOfMonth">
+        <h1>Educational Books</h1>
+        <div id="books">
+            <div>Most Rated</div>
+            <div>New Writers</div>
+            <div>Novels</div>
+            <div>Language</div>
+            <div>Education</div>
+            <div>Fiction</div>
+        </div>
+</div>
+<div id="BookOfMonth">
+    <h1>Language Books</h1>
+    <div id="books">
+        <div>Most Rated</div>
+        <div>New Writers</div>
+        <div>Novels</div>
+        <div>Language</div>
+        <div>Education</div>
+        <div>Fiction</div>
+    </div>
+</div>
+</div>                                          
+
+
+
+<div id="scrollup">
+    <a href="#"><img src="images/scroll.jpg"></a>
+</div>
+
+<div class="footer" >                                    <!--Footer-->
+
+        <div>
+            <span>
+                <p>Join our shelf program</p><br>
+            </span>
+            <p>Get books recomendation and access to our newsletter</p><br>
+            <input type="email" name="email" placeholder=" your@email.com">
+            <button>Subscribe</button>
+        </div>
+        <div id="footerTable">
+        <table>
+            <tr>
+                <td>
+                    <a href="aboutus.html">About Us</a><br><br>
+                    <a href="aboutus.html">Project BookShelf</a>
+                </td>
+                <td>
+
+                </td>
+                <td>
+                <a href="mailto:consfreq@yahoo.com">Have a query</a><br><br>
+                <a href="">Contact Us</a>
+                </td>
+            </tr>
+        </table>
+        </div>
+    <hr>
+            <br>
+            Need our Help !!<br><br>
+        <span>
+            we are available on <a href="mailto:consfreq@yahoo.com">consfreq@yahoo.com</a><br><br>
+        </span>
+        <br>
+           <marquee style="color: rgb(240, 250, 101);" width=50%><cite > * Website is under construction. </cite></marquee> <!--project not complete-->
+</div>
+
+<script src="funtion/funtion3.js">
+</script>
+
+
+</body>
+
+</html>                                        <!--added span in some places to give gap-->
